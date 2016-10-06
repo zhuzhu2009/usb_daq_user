399188d96d56df1b34adf54e49b52c413015e807
zhuzhu2009 <zhuce19940828@me.com>
2016-10-06 14:34:10 +0800

    first commit!

---
+++ src/usb_daq_user.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : usb_daq_user.cpp
+// Author      : zhuce
+// Version     :
+// Copyright   : esss
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
